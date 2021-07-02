58bd29131377a8b4d18ad773735cff1c5add8ebf
Ivan Mamani <ivan.mamani.q@ucb.edu.bo>
2021-07-02 10:59:14 -0400

    Subiendo Proyecto Waliki

---
+++ src/test/java/bo/edu/ucb/ingsoft/WalikiBackend/WalikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.WalikiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalikiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
